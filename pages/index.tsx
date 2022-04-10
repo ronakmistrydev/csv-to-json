8dbc6108a6156e32ad75fc20381e919fe96e1ed4
Ronak Mistry <ronakmistry36@gmail.com>
2022-04-10 01:00:32 -0500

    Basic UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { Blob } from 'buffer';
 import { useState } from 'react';
 import JSONPretty from 'react-json-pretty';
-import { Button } from '@mui/material';
+import {Box, Button, Card, CardActions, CardContent, Container, Grid, Paper, Typography} from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 const Input = styled('input')({
@@ -30,35 +30,84 @@ function HomePage() {
   }
 
   return (
-      <div>
-        <form onSubmit={handleOnSubmit}>
-            <label htmlFor='contained-button-file'>
-                <Input
-                    accept='text/csv'
-                    id='contained-button-file'
-                    multiple
-                    onChange={handleFileOnChange}
-                    type='file'
-                />
-                <Button variant='contained' component='span'>
-                    Upload CSV
-                </Button>
-            </label>
+      <Box
+          sx={{
+              backgroundColor: 'text.secondary',
+              height: '100vh',
+              width: '100vw',
+              display: 'flex',
+              alignItems: 'center'
+          }}
+      >
+          <Container maxWidth='md'>
+              <Typography
+                  gutterBottom
+                  variant='h5'
+                  sx={{
+                      textTransform: 'uppercase',
+                      textAlign: 'center',
+                      color: '#f2f2f2',
+                  }}
+              >
+                  CSV TO JSON Convertor
+              </Typography>
 
-            <Button
-                type='submit'
-                variant='outlined'
-                color='secondary'
-            >
-                Submit
-            </Button>
-        </form>
-          {
-              convertedJson && (
-                  <JSONPretty id='json-pretty' data={convertedJson} />
-              )
-          }
-      </div>
+              <Paper
+                  sx={{
+                      padding: 2
+                  }}
+              >
+                  <form onSubmit={handleOnSubmit}>
+                      <Grid
+                          container
+                          alignItems='center'
+                          justifyContent='space-between'
+                      >
+                          <Grid item xs>
+                              <label htmlFor='contained-button-file'>
+                                  <Input
+                                      accept='text/csv'
+                                      id='contained-button-file'
+                                      multiple
+                                      onChange={handleFileOnChange}
+                                      type='file'
+                                  />
+                                  <Button variant='contained' component='span'>
+                                      Upload CSV
+                                  </Button>
+                                  {
+                                      file && (
+                                          <Typography
+                                              sx={{
+                                                  display: 'inline-block',
+                                                  marginLeft: 2,
+                                              }}
+                                          >
+                                              {file.name}
+                                          </Typography>
+                                      )
+                                  }
+                              </label>
+                          </Grid>
+                          <Grid item xs>
+                              <Button
+                                  type='submit'
+                                  variant='outlined'
+                                  color='secondary'
+                              >
+                                  Submit
+                              </Button>
+                          </Grid>
+                      </Grid>
+                  </form>
+              </Paper>
+              {
+                  convertedJson && (
+                      <JSONPretty id='json-pretty' data={convertedJson} />
+                  )
+              }
+          </Container>
+      </Box>
   )
 }
 
